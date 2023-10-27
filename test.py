96b6e58c5b9ae5629bdcf5af92bb7e491b63b12d
jzhoujg <774780180@qq.com>
2023-10-27 13:47:57 +0800

    1027

--- test.py
+++ test.py
@@ -2,33 +2,13 @@ import scipy.io as sio
 from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
 import torch
-import torch.nn as nn
 from torch.autograd import Variable
-import torch.nn.functional as F
+from models.apl import *
 import matplotlib.pyplot as plt
-import math
-import time
-import torch
-from torch import nn
-from torch.autograd import Variable
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.io as sio
-from torch.utils.data import TensorDataset, DataLoader
-import numpy as np
-import torch
-import torch.nn as nn
-from torch.autograd import Variable
-import torch.nn.functional as F
-import matplotlib.pyplot as plt
-import math
-import time
-
-from tqdm import tqdm
-
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
 
 batch_size = 512
-
 data_amp = sio.loadmat('data/test_data_split_amp.mat')
 test_data_amp = data_amp['test_data']
 test_data = test_data_amp
@@ -50,42 +30,72 @@ test_label = torch.from_numpy(test_label).type(torch.LongTensor)
 test_dataset = TensorDataset(test_data, test_label)
 test_data_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
-aplnet = torch.load('weights/net1111_Train100.0Test88.129Train99.910Test95.683.pkl')
-aplnet = aplnet.cuda().eval()
-
+aplnet = ResNet(block=BasicBlock, layers=[1, 1, 1, 1], inchannel=52)
+state_dict=torch.load('results_weight/awl_84.pth')
+aplnet.load_state_dict(state_dict)
+aplnet = aplnet.eval()
 
 correct_test_loc = 0
 correct_test_act = 0
 
+
+act_matrix = np.zeros((6,6),dtype=int)
+loc_matrix = np.zeros((16,16),dtype=int)
+
+
 for i, (samples, labels) in enumerate(test_data_loader):
     with torch.no_grad():
-        samplesV = Variable(samples.cuda())
+        samplesV = Variable(samples)
         labels_act = labels[:, 0].squeeze()
         labels_loc = labels[:, 1].squeeze()
-        labelsV_act = Variable(labels_act.cuda())
-        labelsV_loc = Variable(labels_loc.cuda())
+        labelsV_act = Variable(labels_act)
+        labelsV_loc = Variable(labels_loc)
 
         predict_label_act, predict_label_loc,_,_,_,_,_,_,_ = aplnet(samplesV)
-
-        # for tsne visualization
-        # act1, loc1, x, c1, c2, c3, c4, act, loc = aplnet(samplesV)
-        # sio.savemat('vis/fig_tsne/out_act_conf.mat', {'out_max': act1.view(act1.shape[0], -1).cpu().numpy()})
-        # sio.savemat('vis/fig_tsne/out_loc_conf.mat', {'out_max': loc1.view(loc1.shape[0], -1).cpu().numpy()})
-        # sio.savemat('vis/fig_tsne/out_maxpool.mat', {'out_max': x.view(x.shape[0], -1).cpu().numpy()})
-        # sio.savemat('vis/fig_tsne/out_c1.mat', {'out_max': c1.view(c1.shape[0], -1).cpu().numpy()})
-        # sio.savemat('vis/fig_tsne/out_c2.mat', {'out_max': c2.view(c2.shape[0], -1).cpu().numpy()})
-        # sio.savemat('vis/fig_tsne/out_c3.mat', {'out_max': c3.view(c3.shape[0], -1).cpu().numpy()})
-        # sio.savemat('vis/fig_tsne/out_c4.mat', {'out_max': c4.view(c4.shape[0], -1).cpu().numpy()})
-        # sio.savemat('vis/fig_tsne/out_fc_act.mat', {'out_max': act.view(act.shape[0], -1).cpu().numpy()})
-        # sio.savemat('vis/fig_tsne/out_fc_loc.mat', {'out_max': loc.view(loc.shape[0], -1).cpu().numpy()})
-
         prediction = predict_label_act.data.max(1)[1]
-        sio.savemat('vis/actResult.mat',{'act_prediction':prediction.cpu().numpy()})
+
         correct_test_act += prediction.eq(labelsV_act.data.long()).sum()
+
+        for i in range(len(prediction)):
+            act_matrix[labelsV_act.data[i]][prediction[i]] += 1
+
         print(correct_test_act.cpu().numpy()/num_test_instances)
 
         prediction = predict_label_loc.data.max(1)[1]
-        sio.savemat('vis/locResult.mat', {'loc_prediction': prediction.cpu().numpy()})
+        for i in range(len(prediction)):
+            loc_matrix[labelsV_loc.data[i]][prediction[i]] += 1
+
         correct_test_loc += prediction.eq(labelsV_loc.data.long()).sum()
         print(correct_test_loc.cpu().numpy() / num_test_instances)
 
+print(loc_matrix)
+print(act_matrix)
+
+
+y_true = np.array(['Positive', 'Negative', 'Positive', 'Positive', 'Negative'])
+y_pred = np.array(['Positive', 'Positive', 'Negative', 'Positive', 'Negative'])
+
+# 创建混淆矩阵
+cm = confusion_matrix(y_true, y_pred)
+
+# 定义类别标签
+
+
+# labels = ["loc"+str(i) for i in range(16)]
+
+labels = ['up','down','left','right','circle','cross']
+
+# 创建热力图
+
+plt.rcParams['font.size'] = 14
+sns.heatmap(act_matrix/45, annot=True, fmt='.2f', cmap='Blues', xticklabels=labels, yticklabels=labels)
+# sns.heatmap(loc_matrix/18, annot=True, fmt='.2f', cmap='Blues', xticklabels=labels, yticklabels=labels)
+# 设置图形属性
+plt.title('Confusion Matrix')
+plt.xlabel('Predicted')
+plt.ylabel('True')
+# 显示混淆矩阵
+plt.show()
+
+dic = {'act_matrix':act_matrix,'loc_matrix':loc_matrix}
+sio.savemat('conmatrix_dwa_92_97.mat',dic)
